b50212d316fe0e180fd6c388f99b71f6b4d94110
Nathan Duvoid <nduvoid@k0r4p11.42mulhouse.fr>
2024-07-31 18:07:55 +0200

    tkt bro

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # Fonction pour ouvrir un terminal et exécuter une commande
 open_terminal_with_command() {
-
-	gnome-terminal -- bash -c "$1; exec bash" #, "$2; exec bash","$3; exec bash","$4; exec bash"
+	commands="curl parrot.live; sleep 10; kill; cat ./install/troll.txt exec bash"
+	gnome-terminal -- bash -c "$1; $2; $3; $4; exec bash"
 }
 
+open_terminal_with_command
 # Installer les modules nécessaires
 pip install pyautogui -qqq
 pip install pywebview -qqq
 pip install Xlib -qqq
 # Ouvrir un terminal et exécuter la commande curl
-open_terminal_with_command "curl parrot.live", "sleep 10", "kill $CURL_PID", "cat ./install/troll.txt"
 
